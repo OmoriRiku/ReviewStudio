f0b6b5e6143af584f9b89d15094c226fe5ef98c0
OmoriRiku <oomori3321@gmail.com>
2023-04-07 07:36:25 +0000

    Add ユーザー新規登録、ログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,41 +1,57 @@
 Rails.application.routes.draw do
-  # 管理者用
-  # URL /customers/sign_in ...
-  devise_for :admin, controllers: {
-    sessions: "admin/sessions"
-  }
-  
-  scope module: :admin do
-    resources :stores do
-      resources :store_reviews, only: [:destroy]
-    end
-    resources :studios do
-      resources :studio_reviews, only: [:destroy]
-    end
-  end
-  
-  scope module: :public do
-    root to: 'homes#top'
-    get '/about' => 'homes#about'
-    resources :uses, only: [:index, :edit]
-    post    '/stores/:store_id/store_reviews'         =>  'store_reviews#create',   as:  'user_store_store_review'
-    delete  '/stores/:store_id/store_reviews/:id'     =>  'store_reviews#destroy'
-    get     '/studios/:studio_id/review_studios/new'  =>  'studio_reviews#new',     as:  'user_new_studio_studio_reviews'
-    post    '/studios/:studio_id/review_studios'      =>  'studio_reviews#create',  as:  'user_studio_studio_reviews'
-    delete  '/studios/:studio_id/review_studios/:id'  =>  'studio_reviews#destroy'
-    get     'search/studios'                          =>  'searches#search_studio'
-    get     'search/studios'                          =>  'searches#search_use'
-  end
-  
   # ユーザー用
   # URL /users/sign_in ...
-  devise_for :end_users, path: "/users", controllers: {
+  devise_for :end_users,skip: [:passwords], path: "/users", controllers: {
     registrations: "public/registrations",
     sessions: 'public/sessions'
   }
-  
+  ## ファイルパスをpublic/... へ
+  #  urlを任意に指定する
   scope module: :public do
+    # ユーザー編集用のルーティング
+    # URL /users/...
     resources :end_users, only: [:edit, :update], path: 'users'
+    # トップページのルーティング
+    # URL /
+    root to: 'homes#top'
+    # aboutページのルーティング
+    # URL /about
+    get '/about' => 'homes#about'
+    # 用途のルーティング
+    # URL /uses/...
+    resources :uses, only: [:index, :edit]
+    # 店舗レビュー用のルーティング
+    post    '/stores/:store_id/store_reviews'         =>  'store_reviews#create',   as:  'user_store_store_review'
+    delete  '/stores/:store_id/store_reviews/:id'     =>  'store_reviews#destroy'
+    # スタジオレビューのルーティング
+    get     '/studios/:studio_id/review_studios/new'  =>  'studio_reviews#new',     as:  'user_new_studio_studio_reviews'
+    post    '/studios/:studio_id/review_studios'      =>  'studio_reviews#create',  as:  'user_studio_studio_reviews'
+    delete  '/studios/:studio_id/review_studios/:id'  =>  'studio_reviews#destroy'
+    # 検索用ルーティング
+    get     'search/studios'                          =>  'searches#search_studio'
+    get     'search/studios'                          =>  'searches#search_use'
+  end
+  # 管理者用
+  # URL /customers/sign_in ...
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+  
+  ## ファイルパスをadmin/...へ
+  #  urlを任意に指定する
+  scope module: :admin do
+    # 店舗のルーティング
+    # 店舗レビューの削除用に入れ子にする
+    # URL /stores/....
+    resources :stores do
+      resources :store_reviews, only: [:destroy]
+    end
+    # スタジオのルーティング
+    # スタジオレビューの削除用に入れ子にする
+    # URL /studios/...
+    resources :studios do
+      resources :studio_reviews, only: [:destroy]
+    end
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
