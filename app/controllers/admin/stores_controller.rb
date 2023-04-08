04a6c0db316243723263adfb9c3cea3474110d81
OmoriRiku <oomori3321@gmail.com>
2023-04-08 11:50:11 +0000

    Add 店舗機能

--- app/controllers/admin/stores_controller.rb
+++ app/controllers/admin/stores_controller.rb
@@ -1,22 +1,60 @@
 class Admin::StoresController < ApplicationController
+  before_action :is_admin_login_authenticate, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_store, only: [:show, :edit, :update, :destroy]
+  
+  ## 新規登録ページ
   def new
+    @store = Store.new
   end
   
+  ## 新規登録機能
   def create
+    @store = Store.new(store_params)
+    @store.save
+    redirect_to store_path(@store.id)
   end
 
+  ## 一覧ページ
   def index
+    @stores = Store.all
   end
 
+  ## 詳細ページ
   def show
   end
 
+  ## 編集ページ
   def edit
   end
 
-  def updatet
+  ## 更新機能
+  def update
+    @store.update(store_params)
+    redirect_to store_path(@store)
   end
 
+  ## 削除機能
   def destroy
+    @store.destroy
+    redirect_to stores_path
+  end
+  
+  private
+  
+  ## Storeモデルから指定のIDを取得するメソッド
+  def set_store
+    @store = Store.find(params[:id])
+  end
+  
+  ## 管理者がログインしていなければトップページへ遷移するメソッド
+  def is_admin_login_authenticate
+    unless admin_signed_in?
+      redirect_to root_path
+    end
+  end
+  
+  ## ストロングパラメーター
+  def store_params
+    params.require(:store).permit(:name, :telephone_number, :store_image, :introduction)
   end
 end
--- app/models/store.rb
+++ app/models/store.rb
@@ -1,6 +1,14 @@
 class Store < ApplicationRecord
   has_one_attached  :store_image      # 店舗の写真のカラム
   
+  def get_store_image(width, height)
+  unless store_image.attached?
+    file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+    store_image.attach(io: File.open(file_path), filename: 'no_image.jpg', content_type: 'image/jpeg')
+  end
+  store_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
   has_many  :studio                   # スタジオとのリレーション
                                       # 店舗はスタジオを複数持っている
                                       
