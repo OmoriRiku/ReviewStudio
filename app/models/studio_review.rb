14c39c1a5e56dae88417b1bbef337a2728c5d3a2
OmoriRiku <oomori3321@gmail.com>
2023-04-13 06:40:42 +0000

    Finish レイアウト

--- app/models/studio_review.rb
+++ app/models/studio_review.rb
@@ -1,13 +1,14 @@
 class StudioReview < ApplicationRecord
-  validates :rate, presence: true
-  validates :number_studio, presence: true
-  validates :comment, presence: true, length: { maximum: 400 }
-  belongs_to :end_user      ## ユーザーとのリレーション
-                            ## ユーザーはスタジオレビューを複数持っている
+  validates :rate, presence: true                                   ## 評価のバリデーション、未入力時エラー
+  validates :number_studio, presence: true                          ## 利用人数のバリデーション、未入力時エラー
+  validates :comment, presence: true, length: { maximum: 400 }      ## レビュー本文のバリデーション、未入力時エラー
   
-  belongs_to :studio        ## スタジオとのリレーション
-                            ## スタジオはスタジオレビューを複数持っている
+  belongs_to :end_user                                              ## ユーザーとのリレーション
+                                                                    ## ユーザーはスタジオレビューを複数持っている
+  
+  belongs_to :studio                                                ## スタジオとのリレーション
+                                                                    ## スタジオはスタジオレビューを複数持っている
                             
-  belongs_to :use           ## 用途とのリレーション
-                            ## 用途はスタジオレビューを複数持っている
+  belongs_to :use                                                   ## 用途とのリレーション
+                                                                    ## 用途はスタジオレビューを複数持っている
 end
--- test/controllers/public/studio_reviews_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class Public::StudioReviewsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
