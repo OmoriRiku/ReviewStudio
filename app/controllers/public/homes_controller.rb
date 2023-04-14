a3323f97b6c24b86a8ab116043b46ed054e6e8bd
OmoriRiku <oomori3321@gmail.com>
2023-04-14 09:13:37 +0000

    Add 店舗に郵便番号、住所のカラム追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,8 @@
 class Public::HomesController < ApplicationController
   def top
-    @studios = Studio.limit(12).order(created_at: :desc)     ## 最新12件を取得し最後に登録した順に並び替える
+    @studios = Studio.limit(8).order(created_at: :desc)     ## 最新12件を取得し最後に登録した順に並び替える
+    @stores = 
+    @uses = Use.all
   end
 
   def about                                                 ## aboutページ
